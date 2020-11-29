6ee033bd40d2d15821f190a5f87827dd6cb82c6a
Yogesh Kumar <yogeshkumar98103@hotmail.com>
2020-11-29 21:43:39 +0530

    fixed 32 bit code

--- os/build/constants.mk
+++ os/build/constants.mk
@@ -1,10 +1,10 @@
-# RASPI_MODEL = 2
 OS_INC_DIR = include
 
 ifeq ($(RASPI_MODEL), 4)
 	# Raspberry Pi 4 
 
 	AARCH = aarch64
+	CXX = aarch64-none-elf-cc
 	CPU = cortex-a72
 	DIRECTIVES = -D AARCH64
 	MFPU = crypto-neon-fp-armv8
@@ -14,6 +14,7 @@ else ifeq ($(RASPI_MODEL), 2)
 	# Raspberry Pi 2
 
 	AARCH = aarch32
+	CXX = arm-none-eabi-g++
 	CPU = cortex-a7
 	DIRECTIVES = -D AARCH32
 	MFPU = neon-vfpv4
@@ -22,8 +23,8 @@ else
 
 endif
 
-CXX = arm-none-eabi-g++
-ARCHFLAGS = -mcpu=${CPU} -mfpu=${MFPU} -mfloat-abi=hard -march=${MARCH}
+# -march=${MARCH}
+ARCHFLAGS = -mcpu=${CPU} -mfpu=${MFPU} -mfloat-abi=hard 
 CXXFLAGS = -fpic -ffreestanding -fno-exceptions ${DIRECTIVES}
 CXXSRCFLAGS = -O2 -Wall -Wextra
 LFLAGS = -ffreestanding -O2 -nostdlib
