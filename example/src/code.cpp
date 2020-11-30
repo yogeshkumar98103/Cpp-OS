e740f9ccb7b6f52a1f692b533f92bb10bd369c56
Yogesh Kumar <yogeshkumar98103@hotmail.com>
2020-11-30 15:32:36 +0530

    fixed few 64 bit errors

--- example/src/code.cpp
+++ example/src/code.cpp
@@ -2,8 +2,15 @@
 #include "os/console.h"
 #include "os/memory.h"
 
+#ifdef AARCH64 
+extern "C" int entry(uint64_t dtb_ptr32, uint64_t x1, uint64_t x2, uint64_t x3){
+    os::kernel::init(dtb_ptr32, x1, x2, x3);
+
+#else
 extern "C" int entry(int r1, int r2, int atags){
     os::kernel::init(r1, r2, atags);
+
+#endif
 
     void* heap_start   = os::memory::get_kernel_end();
     uint32_t heap_size = os::memory::get_memory_size();
